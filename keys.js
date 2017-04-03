e9bd53581fce6beac10653da7e4717954967ee26
Kelly Davis <kellydavis1974@gmail.com>
2017-04-03 19:37:19 -0400

    set up reqires, installed packages, package.json

---
+++ keys.js
@@ -0,0 +1,8 @@
+var config = JSON.parse(process.env.HEROKU_CONFIG);
+
+exports.twitterKeys = {
+  consumer_key: config.CONSUMER_KEY,
+  consumer_secret: config.CONSUMER_SECRET,
+  access_token_key: config.ACCESS_TOKEN_KEY,
+  access_token_secret: config.ACCESS_TOKEN_SECRET
+}
---
+++ liri.js
@@ -0,0 +1,13 @@
+// grab keys for twitter api
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+var inquirer = require("inquirer");
+/* commands are:
+my-tweets
+spotify-this-song
+movie-this
+do-what-it-says
+*/
+
