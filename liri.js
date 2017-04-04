503ef59cc21e17c2907a0b9848469d7acd364381
Kelly Davis <kellydavis1974@gmail.com>
2017-04-03 20:22:26 -0400

    switch for user choices, including an exit option

--- liri.js
+++ liri.js
@@ -12,19 +12,58 @@ do-what-it-says
 */
 
 // start with an inquirer prompt for LIRI's action to take
-var choice = {
+var q1 = {
         type: "list",
         message: "Ask LIRI to ... ",
         choices: [
             "show my latest Tweets",
             "provide Spotify a song",
             "spill it about a movie",
-            "do whatever it wants"
+            "do whatever it wants",
+            "go away"
         ],
         name: "choice"
     };
 inquirer.prompt([
-    choice
-]);
+    q1
+    // the promise callback checks the choice object (sent in as'input') with a switch
+]).then(function(input) {
+    console.log(input.choice);
+    switch (input.choice) {
+        case "go away":
+            console.log("Bye Felicia!");
+            break;
+        case "show my latest Tweets":
+            fTwitter();
+            break;
+        case "provide Spotify a song":
+            fSpotify();
+            break;
+        case "spill it about a movie":
+            fOMDB();
+            break;
+        case "do whatever it wants":
+            fRandom();
+            break;
+    }
+});
 
+// twitter function 
+function fTwitter(){
+    console.log("Twitter stuff");
+}
 
+// spotify function
+function fSpotify(){
+    console.log("Spotify stuff");
+}
+
+// OMDB function
+function fOMDB(){
+    console.log("OMDB stuff");
+}
+
+// random runction
+function fRandom(){
+    console.log("Random stuff");
+}
