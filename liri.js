0b734a67234dfbabc075829004dc1096bdb7aa3a
Kelly Davis <kellydavis1974@gmail.com>
2017-04-03 20:02:14 -0400

    created user input choice object

--- liri.js
+++ liri.js
@@ -11,3 +11,20 @@ movie-this
 do-what-it-says
 */
 
+// start with an inquirer prompt for LIRI's action to take
+var choice = {
+        type: "list",
+        message: "Ask LIRI to ... ",
+        choices: [
+            "show my latest Tweets",
+            "provide Spotify a song",
+            "spill it about a movie",
+            "do whatever it wants"
+        ],
+        name: "choice"
+    };
+inquirer.prompt([
+    choice
+]);
+
+
